6fbce033396c775c152cdd04ee7d00d5bd94c311
Sanchit Batra <sanchitcop19@gmail.com>
2021-09-23 19:27:44 -0700

    Add eslint and modularize module

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # virtual-coffee
 
-This repository contains a minimal slackbot that attempts to replicate some of donut's functionality (for free). The purpose is to schedule 1:1 conversations between people in the same workspace. It can currently only be run locally and doesn't do match except create conversations with unique pairs. 
+This repository contains a minimal slackbot that attempts to replicate some of donut's functionality (for free). The purpose is to schedule 1:1 conversations between people in the same workspace. It can currently only be run locally and doesn't do match except create conversations with unique pairs.
 
 ## Instructions
+
+You'll need to create a \_\_secrets\_\_.js file. This file contains the variables SLACK_SIGNING_SECRET, SLACK_BOT_TOKEN and SLACK_APP_TOKEN. These variables are available in your Slack API console, refer to Slack's documentation for information on where to find them.
 
 From the base of the repo, execute:
 
 `node index.js`
 
-This starts a server on port 3100. The server currently listens for a message containing the text "pair", which triggers the pairing of individuals. This message can be sent in any channel the bot is a part of.  
+This starts a server on port 3100. The server currently listens for a message containing the text "pair", which triggers the pairing of individuals. This message can be sent in any channel the bot is a part of.
 
-The handler stores pairings in a remote DB so that the same people are not matched again. 
+The handler stores pairings in a remote DB so that the same people are not matched again.
 
 ## Coming next
 
 - Deployment to slack app store
 - GCal integration
 - Zoom integration
-
--- index.js
+++ index.js
@@ -1,52 +1,118 @@
-// @ts-nocheck
+/* eslint-disable no-await-in-loop */
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-shadow */
+/* eslint-disable no-continue */
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-use-before-define */
+/* eslint-disable quotes */
 const { App } = require("@slack/bolt");
 const _ = require("lodash");
-const secrets = require("./__secrets__");
-const _request = require("request");
-const util = require("util");
 const fs = require("fs");
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-const request = util.promisify(_request);
+const secrets = require("./__secrets__");
 const store = require("./store.json");
 
-function storePair({ pair }) {
-  const body = { pair: [pair[0], pair[1]] };
-  return body;
+async function handler() {
+  const app = new App({
+    token: secrets.SLACK_BOT_TOKEN,
+    socketMode: true,
+    appToken: secrets.SLACK_APP_TOKEN,
+    signingSecret: secrets.SLACK_SIGNING_SECRET,
+  });
+
+  app.message("pair", async (args) => {
+    const { say, client } = args;
+
+    const users = await client.users.list();
+    const filteredUsers = users.members.filter(
+      (user) => !user.deleted && !user.is_bot && user.is_email_confirmed
+    );
+
+    const { pairs, existingPairs } = generatePairs({ users: filteredUsers });
+
+    const pairInstances = await initiateConversation({ pairs, client, say });
+
+    const filename = `${__dirname}/store.json`;
+    fs.writeFileSync(
+      filename,
+      JSON.stringify([...pairInstances, ...existingPairs])
+    );
+  });
+  return app;
 }
 
-async function getPairs() {
-  return store;
-}
+async function initiateConversation({ pairs, client, say }) {
+  const res = [];
+  for (const pair of pairs) {
+    res.push(getPairInstance({ pair }));
 
-function alreadyPaired({ history, pair }) {
-  for (const paired of history) {
-    if (
-      _.isEqual(
-        new Set(paired.map((pair) => pair.id)),
-        new Set(pair.map((p) => p.id))
-      )
-    ) {
-      return true;
-    }
+    await say({
+      text: "New pair created",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            text: `Pairing ${pair[0].name} and ${pair[1].name}`,
+            type: "mrkdwn",
+          },
+        },
+      ],
+    });
+
+    const { channel } = await client.conversations.open({
+      users: pair.map((p) => p.id).join(","),
+    });
+
+    await client.chat.postMessage({
+      channel: channel.id,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hi ${pair[0].name} and ${pair[1].name}! You have been paired for today's virtual coffee round, go ahead and schedule a time to chat!`,
+          },
+        },
+      ],
+    });
   }
-  return false;
+  return res;
+}
+
+function generatePairs({ filteredUsers }) {
+  const existingPairs = getMatchedPairs();
+  return {
+    pairs: createPairs({
+      members: filteredUsers,
+      history: existingPairs.map((obj) => obj.pair),
+    }),
+    existingPairs,
+  };
 }
 
 function createPairs({ members, history }) {
   const pairs = [];
   const paired = new Set();
+
   for (const member1 of members) {
     for (const member2 of members) {
+      // skip if we are looking at the same user or
+      // if the user been paired with somebody else in this round
       if (
         member1.id === member2.id ||
         paired.has(member1.id) ||
         paired.has(member2.id)
-      )
+      ) {
         continue;
+      }
+
       const pair = [
         { id: member1.id, name: member1.profile.real_name },
         { id: member2.id, name: member2.profile.real_name },
       ];
+
       if (!alreadyPaired({ history, pair })) {
         pairs.push(pair);
         paired.add(pair[0].id);
@@ -59,64 +125,21 @@ function createPairs({ members, history }) {
   return pairs;
 }
 
-// @ts-ignore
-async function handler(local = false) {
-  const app = new App({
-    token: secrets.SLACK_BOT_TOKEN,
-    socketMode: true,
-    appToken: secrets.SLACK_APP_TOKEN,
-    signingSecret: secrets.SLACK_SIGNING_SECRET,
-  });
-  app.message("pair", async (args) => {
-    const { say, client } = args;
-    const users = await client.users.list();
-    const filteredUsers = users.members.filter(
-      // @ts-ignore
-      (user) => !user.deleted && !user.is_bot && user.is_email_confirmed
-    );
-    const existingPairs = await getPairs();
-    // const existingPairs = [];
-    const pairs = createPairs({
-      members: filteredUsers,
-      history: existingPairs.map((obj) => obj.pair),
-    });
+function alreadyPaired({ history, pair }) {
+  return history.some((historyPair) =>
+    _.isEqual(
+      new Set(historyPair.map((pair) => pair.id)),
+      new Set(pair.map((p) => p.id))
+    )
+  );
+}
 
-    const res = [];
-    for (const pair of pairs) {
-      res.push(storePair({ pair }));
-      //   await delay(1500);
-      await say({
-        text: "New pair created",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              text: `Pairing ${pair[0].name} and ${pair[1].name}`,
-              type: "mrkdwn",
-            },
-          },
-        ],
-      });
-      const { channel } = await client.conversations.open({
-        users: pair.map((p) => p.id).join(","),
-      });
-      await client.chat.postMessage({
-        channel: channel.id,
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `Hi ${pair[0].name} and ${pair[1].name}! You have been paired for today's virtual coffee round, go ahead and schedule a time to chat!`,
-            },
-          },
-        ],
-      });
-    }
-    const filename = __dirname + "/store.json";
-    fs.writeFileSync(filename, JSON.stringify([...res, ...existingPairs]));
-  });
-  return app;
+function getPairInstance({ pair }) {
+  return { pair: [pair[0], pair[1]] };
+}
+
+function getMatchedPairs() {
+  return store;
 }
 
 (async () => {
